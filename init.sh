ff26d716e31cde860cef75ecd6e387c9156f0d1a
Marco Triglia <marcotriglia@users.noreply.github.com>
2016-10-08 16:06:55 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,29 +1,100 @@
 #!/bin/bash
 
+# TODO: Set up Git for user (ask name and email)
+# TODO: Create new user
+
+# Options
+# d - domain (optional): specify domain name (otherwise connect using ip address)
+# s - ssl (flag, optional): get ssl certificate and configure server to use it (MUST HAVE DOMAIN)
+# p - port (optional): specify what port you want to use
+
 #Check if the script is being run as root
 if [ "$(whoami)" != "root" ]; then
-	echo "You must run this script as root."
-	exit 1
+	echo "You must run this script as root.";
+	exit 1;
 fi
 
 # Go to home directory
 cd ~
 
+# Set defaults
+domain="";
+ssl=false;
+port=3000;
+
+# Get option arguments
+while getopts d::sp::f flag; do
+  case $flag in
+    d)
+      domain=$OPTARG;
+      ;;
+    s)
+      ssl=true;
+      ;;
+    p)
+      port=$OPTARG;
+      ;;
+    ?)
+      exit;
+      ;;
+  esac
+done
+shift $(( OPTIND - 1 ));
+
+# TODO: Trim domain
+
+# TODO: Validate domain
+
+# If domain is empty, make sure ssl is set to false
+if [ -z "$domain" ] && $ssl; then
+	printf "A domain is required in order to set up SSL\n"
+	ssl=false;
+	exit; # Comment this line out to continue executing with ssl set to false
+fi
+
 printf "\nStarting Script\n\n"
+
+# Update and upgrade OS
 apt-get update
 apt-get -y upgrade
+
+# Get node and install it
 curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
 bash nodesource_setup.sh
 apt-get -y install nodejs build-essential nginx letsencrypt
+
+# Install pm2
 npm install -g pm2
 
 # Clone git repo with code
 git clone https://github.com/marcotriglia/ubuntu-node-init.git
+
+# Check if repo was cloned successfully
+if [ -L "ubuntu-node-init" ]; then
+	printf "FATAL ERROR: Unable to clone git repository.\n"
+	exit;
+fi
+
+mv ubuntu-node-init/default /etc/nginx/sites-available/default
+
+# Replace placeholders in nginx config files with right variables
+sed -i "s/your_domain_name/$domain/g" default*
+sed -i "s/your_port/$port/g" default*
+
+# Restart Nginx
+systemctl restart nginx
+
+# Allow traffic to Nginx through firewall
+ufw allow 'Nginx Full'
+
+# Move needed files out of git directory (it will be deleted later)
 mv ubuntu-node-init/hello.js hello.js
 
 # Clean up
 rm nodesource_setup.sh
 rm -r ubuntu-node-init
 
+# Start hello.js via pm2
+pm2 start hello.js
 
 printf "\nInitialization successful\n\n"
